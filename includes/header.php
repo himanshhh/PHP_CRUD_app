4cefe17d61853f8d32d61d4926b96f0010334666
Himansh Arora <d00233455@student.dkit.ie>
2021-02-17 00:14:12 +0000

    implemented scss and little bit nesting

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <!-- the head section -->
 <head>
 <title>My PHP CRUD App</title>
-<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="stylesheet" type="text/css" href="css/demo.css">
 </head>
 
 <!-- the body section -->
